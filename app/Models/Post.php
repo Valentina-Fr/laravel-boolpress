3fc6b2d2db98decf470ee2d803438c269271e40f
Valentina-Fr <v_frasca@virgilio.it>
2021-10-19 15:52:54 +0200

    Fix carbon format

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ use Carbon\Carbon;
 
 class Post extends Model
 {
-    public function getFormattedDate($column, $format = 'd-m-Y H:h:s') {
+    public function getFormattedDate($column, $format = 'd-m-Y H:i:s') {
         return Carbon::create($this->$column)->format($format);
     }
 }
