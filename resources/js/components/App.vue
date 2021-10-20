d43c3f9979464f1c07491b185fc3f5d853dfc9d1
Valentina-Fr <v_frasca@virgilio.it>
2021-10-20 17:08:45 +0200

    Add pagination

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="container mb-5">
+    <div class="container py-3">
         <div class="card my-5" v-for="(post, index) in posts" :key="index">
             <div class="card-body">
                 <h5 class="card-title">{{ post.title }}</h5>
                 <p class="card-text">{{ post.article }}</p>
-                <a href="#" class="btn btn-primary">Read</a>
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="#" class="btn btn-primary">Read</a>
+                    <small>Published: {{ post.created_at }}</small>
+                </div>
             </div>
         </div>
+        <nav>
+            <ul class="pagination">
+                <li class="page-item" :class="{ active: pages.currentPage === n }" v-for="n in pages.lastPage" :key="n">
+                    <a class="page-link" @click="getPosts(n)">{{ n }}</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
@@ -16,13 +26,18 @@
         data(){
             return {
                 baseUri:'http://127.0.0.1:8000',
-                posts: []
+                posts: [],
+                pages: {}
             }
         },
         methods: {
-            getPosts() {
-                axios.get(`${this.baseUri}/api/posts`)
-                .then((res)=> { this.posts = res.data; })
+            getPosts(page) {
+                axios.get(`${this.baseUri}/api/posts?page=${page}`)
+                .then((res)=> {
+                    const {data, current_page, last_page} = res.data;
+                    this.posts = data;
+                    this.pages = { currentPage: current_page, lastPage: last_page }
+                })
                 .catch((err)=> { console.error(err); });
             }
         }, 
@@ -31,3 +46,9 @@
         }
     }
 </script>
+
+<style scoped>
+    .page-link {
+        cursor: pointer;
+    }
+</style>
